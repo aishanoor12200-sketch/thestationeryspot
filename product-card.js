6e2a151233ca135e3f0dc57a9c148cf195c86eef
aishanoor12200-sketch <aishanoor12200@gmail.com>
2026-09-09 01:48:25 +0500

    Refactor product card HTML for accessibility
    Updated HTML structure in product-card.js to improve accessibility and formatting.

--- product-card.js
+++ product-card.js
@@ -5,26 +5,40 @@ class ProductCard extends HTMLElement {
     const price = this.getAttribute("price") || "0";
     const code = this.getAttribute("code") || "";
 
-    this.outerHTML = `
+    this.innerHTML = `
       <div class="card">
         <div class="img-box">
-          <img src="${image}" loading="lazy" width="150" height="200"
-               onclick="openImage(this.src)">
-          <div class="floating-icon"
-               onclick="addToCart(this,'${code}')">🛒</div>
+          <img
+            src="${image}"
+            alt="${name}"
+            loading="lazy"
+            width="150"
+            height="200"
+            onclick="openImage(this.src)"
+          >
+
+          <div
+            class="floating-icon"
+            onclick="addToCart(this, '${code}')"
+            aria-label="Add ${name} to cart"
+          >
+            🛒
+          </div>
         </div>
 
         <p class="name">${name}</p>
         <p class="price">Rs.${price}</p>
 
         <div class="qty-box">
-          <button onclick="decrease(this)">-</button>
+          <button onclick="decrease(this)">−</button>
           <span class="qty">1</span>
           <button onclick="increase(this)">+</button>
         </div>
 
-        <button class="add-btn"
-                onclick="addToCart(this,'${code}')">
+        <button
+          class="add-btn"
+          onclick="addToCart(this, '${code}')"
+        >
           Add to Cart
         </button>
       </div>
